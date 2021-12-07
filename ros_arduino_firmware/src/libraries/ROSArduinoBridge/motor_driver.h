8e1e095a5c487940aa8c70032e9571ebda846278
yanjingang <yanjingang@baidu.com>
2021-12-07 23:42:05 +0800

    pin

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.h
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.h
@@ -4,9 +4,9 @@
    *************************************************************/
 
 #ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 5  // 
-  #define LEFT_MOTOR_BACKWARD  6  //
-  #define RIGHT_MOTOR_FORWARD  9  //
+  #define RIGHT_MOTOR_BACKWARD 5  //
+  #define RIGHT_MOTOR_FORWARD  6  // 
+  #define LEFT_MOTOR_BACKWARD  9  //
   #define LEFT_MOTOR_FORWARD   10 //
   #define RIGHT_MOTOR_ENABLE 12   //
   #define LEFT_MOTOR_ENABLE 13    //

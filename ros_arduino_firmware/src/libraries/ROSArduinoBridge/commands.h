1d54f5d7ba0991ef97dce47b69627a7f3a7bc8e3
yanjingang <yanjingang@mail.com>
2021-12-08 16:01:06 +0800

    note

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/commands.h
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/commands.h
@@ -6,20 +6,20 @@
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-#define ANALOG_READ    'a'  // 获取模拟引脚上的读数
-#define GET_BAUDRATE   'b'  // 获取固件波特率
+#define ANALOG_READ    'a'  // 获取模拟引脚上的读数（例如a 3）
+#define GET_BAUDRATE   'b'  // 获取波特率
 #define PIN_MODE       'c'  // 设置引脚模式（0 input, 1 output）
-#define DIGITAL_READ   'd'  // 获取数字引脚上的读数
+#define DIGITAL_READ   'd'  // 获取数字引脚上的读数（例如d 4）
 #define READ_ENCODERS  'e'  // 获取电机编码器计数
-#define MOTOR_SPEEDS   'm'  // 以指定的速度移动机器人
+#define MOTOR_SPEEDS   'm'  // 指定速度（例如m 20 20）
 #define PING           'p'  // 
-#define RESET_ENCODERS 'r'
+#define RESET_ENCODERS 'r'  // 复位
 #define SERVO_WRITE    's'  // 设置伺服的位置 -弧度(0-3.14)
 #define SERVO_READ     't'  // 读伺服的位置
-#define UPDATE_PID     'u'  
+#define UPDATE_PID     'u'  // 更新PID
 #define DIGITAL_WRITE  'w'  // 向数字引脚发送低(0)或高(1)信号
 #define ANALOG_WRITE   'x'  // 向模拟引脚发送模拟信号
-#define LEFT            0
+#define LEFT            0   
 #define RIGHT           1
 
 #endif

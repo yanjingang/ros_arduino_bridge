2dceae7493eaaab1d3d54c01296c0c5f659b0bce
yanjingang <yanjingang@mail.com>
2021-12-08 17:22:56 +0800

    pin note

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.h
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.h
@@ -4,12 +4,17 @@
    *************************************************************/
 
 #ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 5  //
-  #define RIGHT_MOTOR_FORWARD  6  // 
-  #define LEFT_MOTOR_BACKWARD  9  //
-  #define LEFT_MOTOR_FORWARD   10 //
-  #define RIGHT_MOTOR_ENABLE 12   //
-  #define LEFT_MOTOR_ENABLE 13    //
+  // 右轮电机驱动写引脚（接L298电驱板input口）
+  #define LEFT_MOTOR_BACKWARD  9
+  #define LEFT_MOTOR_FORWARD   10
+  
+  // 右轮电机驱动写引脚（接L298电驱板input口）
+  #define RIGHT_MOTOR_BACKWARD 5
+  #define RIGHT_MOTOR_FORWARD  6
+
+  // 电机开关（目前没用，可以不接）
+  #define RIGHT_MOTOR_ENABLE 12
+  #define LEFT_MOTOR_ENABLE 13
 #endif
 
 void initMotorController();
